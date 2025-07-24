d7462bf61e6baf40862d94e7c63f18402f33063e
farahmetoui <farah.metoui@esprit.tn>
2025-07-24 16:33:05 +0200

    fixAppToTestController

--- src/routes/LighthouseRouter.ts
+++ src/routes/LighthouseRouter.ts
@@ -1,11 +1,10 @@
 import express from "express"; 
-import {createApplication} from "../../src/controllers/AppToTestController"
+import {createApplication, getAllApplicationsController } from "../../src/controllers/AppToTestController";
 import {getMetricsAverageController, saveMetrics} from "../../src/controllers/MetricsContoller"
-import {getAllApplicationsController} from "../../src/controllers/AppToTestController"
-import { getDetailMetricsController, getTestMetricsById, getWebVitalById } from "@src/controllers/WebVitalController";
-import { getTestAppController, getTestsByMonthController, getTotalTests } from "@src/controllers/TestController";
-import authorization from "@src/middleware/authorization";
-import { paginateTablesController } from "@src/controllers/PaginationController";
+import {getDetailMetricsController, getTestMetricsById, getWebVitalById } from "../../src/controllers/WebVitalController";
+import {getTestAppController, getTestsByMonthController, getTotalTests } from "../../src/controllers/TestController";
+import authorization from "../../src/middleware/authorization";
+import { paginateTablesController } from "../../src/controllers/PaginationController";
 
 
 const router = express.Router();
